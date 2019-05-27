4c0021bd1536c34a6ba001ea2a18489f05a2fdcc
Jeong Ook Moon <moonjeongook@gmail.com>
2019-05-26 23:55:57 -0700

    initilized yay korea project

---
+++ src/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <a href="#">
+        <img src="../../korea.svg" className="logo" />
+      </a>
+    </div>
+  );
+};
+
+export default NavBar;
