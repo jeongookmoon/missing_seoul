0354ce83e07880611a4b8ba8b9e13ec395c62a45
Jeong Ook Moon <jeongook.moon@sjsu.edu>
2019-05-28 01:45:44 -0700

    simplified css for mansory layout

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,26 +1,17 @@
 import React from "react"
 import { IMGS } from "./LoadImgs"
 
-const imgCards = [], imgCards2 = [], imgCards3 = []
-let i = 0
+const imgCards = []
 for (const key in IMGS) {
-  if (i <= 3)
-    imgCards.push(<img src={IMGS[key]} key={key} alt={key} className="landingImg" />)
-  else if (i <= 7)
-    imgCards2.push(<img src={IMGS[key]} key={key} alt={key} className="landingImg" />)
-  else
-    imgCards3.push(<img src={IMGS[key]} key={key} alt={key} className="landingImg" />)
-  i++
+  imgCards.push(<img src={IMGS[key]} key={key} alt={key} className="mansory_item" />)
 }
 
-const Content = () => {
+const Home = () => {
   return (
-    <div className="content">
-      <div className="imgCardCol">{imgCards}</div>
-      <div className="imgCardCol">{imgCards2}</div>
-      <div className="imgCardCol">{imgCards3}</div>
+    <div className="home">
+      <div className="mansory">{imgCards}</div>
     </div>
   )
 }
 
-export default Content
+export default Home
--- src/Home/LoadImgs.js
+++ src/Home/LoadImgs.js
@@ -1,27 +1,29 @@
-import landing0 from "./img/landing0.jpg"
-import landing1 from "./img/landing1.jpg"
-import landing2 from "./img/landing2.jpg"
-import landing3 from "./img/landing3.jpg"
-import landing4 from "./img/landing4.jpg"
-import landing5 from "./img/landing5.jpg"
-import landing6 from "./img/landing6.jpg"
-import landing7 from "./img/landing7.jpg"
-import landing8 from "./img/landing8.jpg"
-import landing9 from "./img/landing9.jpg"
-import landing10 from "./img/landing10.jpg"
-import landing11 from "./img/landing11.jpg"
+import LANDING0 from "./img/landing0.jpg"
+import LANDING1 from "./img/landing1.jpg"
+import LANDING2 from "./img/landing2.jpg"
+import LANDING3 from "./img/landing3.jpg"
+import LANDING4 from "./img/landing4.jpg"
+import LANDING5 from "./img/landing5.jpg"
+import LANDING6 from "./img/landing6.jpg"
+import LANDING7 from "./img/landing7.jpg"
+import LANDING8 from "./img/landing8.jpg"
+import LANDING9 from "./img/landing9.jpg"
+import LANDING10 from "./img/landing10.jpg"
+import LANDING11 from "./img/landing11.jpg"
+import LANDING12 from "./img/landing12.jpg"
 
 export const IMGS = {
-  landing0,
-  landing1,
-  landing2,
-  landing3,
-  landing4,
-  landing5,
-  landing6,
-  landing7,
-  landing8,
-  landing9,
-  landing10,
-  landing11
+  LANDING0,
+  LANDING1,
+  LANDING2,
+  LANDING3,
+  LANDING4,
+  LANDING5,
+  LANDING6,
+  LANDING7,
+  LANDING8,
+  LANDING9,
+  LANDING10,
+  LANDING11,
+  LANDING12
 }
