e1acd5741cfd2f3e1af47c1767e3f764b35a36c7
Jeong Ook Moon <moonjeongook@gmail.com>
2019-05-28 18:57:09 -0700

    updated NavBar UI and responsive CSS

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,19 +1,46 @@
 import React from "react"
+import "./navbar_style.css"
+import { ENG_MENU, KOR_MENU } from "./menu_list"
 
-const NavBar = () => {
-  return (
-    <div className="navbar">
-      <div className="menu">
-        <h1><a href="#">Missing Seoul</a></h1>
-        <ul>
-          <li><a href="#">Attraction</a></li>
-          <li><a href="#">Culture</a></li>
-          <li><a href="#">Food</a></li>
-          <li><a href="#">Post</a></li>
-        </ul>
+class NavBar extends React.Component {
+
+  state = {
+    menu_items: ENG_MENU
+  }
+
+  switchItemLanguage = (event) => {
+    const { name } = event.target
+    const currentLanguage = this.state.menu_items[name]
+    const newItemName = (currentLanguage === ENG_MENU[name]) ? KOR_MENU[name] : ENG_MENU[name]
+    this.setState(prevState => ({
+      menu_items: {
+        ...ENG_MENU,
+        [name]: newItemName
+      }
+    }))
+  }
+
+  render() {
+    const { attraction, culture, food, post } = this.state.menu_items
+    return (
+      <div className="navbar">
+        <div className="menu">
+          <h1><a href="#" className="menu_item menu_title">Missing Seoul</a></h1>
+          <ul>
+            <li><a href="#" className="menu_item menu_list" name="attraction"
+              onMouseLeave={this.switchItemLanguage}>{attraction}</a></li>
+            <li ><a href="#" className="menu_item menu_list" name="culture"
+              onMouseLeave={this.switchItemLanguage}>{culture}</a></li>
+            <li><a href="#" className="menu_item menu_list" name="food"
+              onMouseLeave={this.switchItemLanguage}>{food}</a></li>
+            <li><a href="#" className="menu_item menu_list" name="post"
+              onMouseLeave={this.switchItemLanguage}>{post}</a></li>
+          </ul>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
+
 
 export default NavBar
---
+++ src/NavBar/menu_list.js
@@ -0,0 +1,13 @@
+export const ENG_MENU = {
+  attraction: "Travel",
+  culture: "Culture",
+  food: "Food",
+  post: "Post"
+}
+
+export const KOR_MENU = {
+  attraction: "여행",
+  culture: "문화",
+  food: "음식",
+  post: "포스트"
+}
